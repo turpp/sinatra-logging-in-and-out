e20fa087ebd1a3190dae5892be77ba3344cf3c9f
turpp <white.tripp@icloud.com>
2020-11-11 20:59:13 -0600

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,12 @@
 class Helpers
+    def self.current_user(hsh)
+        @user=User.find(hsh[:user_id])
+        @user
+    end
+
+    def self.is_logged_in?(hsh)
+        
+        !!hsh[:user_id]
+        
+    end
 end
